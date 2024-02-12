5b1d2a3c67e378e0eda2c19f0a2b5bae95643c77
George0118 <georgemystriotis@gmail.com>
2024-02-12 21:54:15 +0200

    Node First Implementation + Transaction Changes

--- Classes/Transaction.py
+++ Classes/Transaction.py
@@ -3,31 +3,41 @@
 import time
 import json
 import hashlib
-import socket
-import pickle
 
 class Transaction:
-    def __init__(self, receiver_address, me, amount, message):
+    def __init__(self, type, receiver_address, me, amount, message):
+
+        self.type = type                            # Set the Transcation Type (Initialization | Stake | Exchange)
         
-        self.receiver_address = receiver_address    # Receiver Address
+        if self.type != "Stake":
+            self.receiver_address = receiver_address    # Receiver Address
+        else:
+            self.receiver_address = 0
 
         self.sender_address = me.publicKey          # Sender Address (MyAddress)
-        self.signature = None              # Signature (Needs to be signed)
+        self.signature = None                       # Signature (Needs to be signed)
         self.nonce = me.counter                     # Nonce
 
-        self.fee = amount*0.03 + len(message)       # Calculate whole amount
-        self.message = message                      # Message (if no message then "")
+        if self.type != "Exchange":
+            self.fee = 0
+            self.message = ""
+        else:
+            self.fee = amount*0.03 + len(message)       # Calculate the fee
+            self.message = message                      # Message (if no message then "")
 
-        self.transaction_id = self.generate_transaction_id      # Based on the characteristics set transaction_id
+        self.amount = amount                        # Amount to send
+
+        self.transaction_id = self.generate_transaction_id()      # Based on the characteristics set transaction_id
 
     def generate_transaction_id(self):
 
         transaction_data = {
+            'type': self.type,
             'receiver': self.receiver_address,
             'sender': self.sender_address,
-            'signature': self.signature,
             'nonce': self.nonce,
             'fee': self.fee,
+            'amount': self.amount,
             'message': self.message,
             'timestamp': time.time()
         }
@@ -41,35 +51,42 @@ class Transaction:
     
     def transaction_signing(self, signature):
         self.signature = signature
+
+    def to_dict(self):
+        """
+        Convert the transaction to a dictionary, without signature.
+        """
+        return {
+            "sender_address": self.sender_address,
+            "receiver_address": self.receiver_address,
+            "fee": self.fee,
+            "amount": self.amount,
+            "message": self.message,
+            "type_of_transaction": self.type,
+            "nonce": self.nonce,
+            "transaction_id": self.transaction_id,
+            "signature": self.signature,
+        }
+
+    def equals(self, transaction):
+        """
+        Check if two transactions are equal.
+        """
+        return self.transaction_id == transaction.transaction_id
     
-    def broadcast_transaction(self, communication_info):
-        if(self.signature == None):         # Check if Transcation is signed
-            print("Transaction not signed! Can't broadcast...\n")
-        else:
-            for IP_address, Port in communication_info:         # If it is then broadcast to everyone
-                self.send_transaction(IP_address, Port) 
-            print("Transcation broadcasted successfully\n")
-
-    def send_transaction(self, IP_address, Port):
-        try:
-            # Create a socket object
-            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            
-            # Connect to the server
-            client_socket.connect((IP_address, Port))
-
-            # Serialize the transaction using pickle
-            serialized_transaction = pickle.dumps(self)
-
-            # Send the serialized transaction
-            client_socket.send(serialized_transaction)
-
-        except Exception as e:
-            print(f"Error broadcasting transaction: {e}\n")
-
-        finally:
-            # Close the socket
-            client_socket.close()
-
+    def payload(self):
+        """
+        Generate the payload for the transaction.
+        """
+        return {
+            'type': self.type,
+            'receiver': self.receiver_address,
+            'sender': self.sender_address,
+            'nonce': self.nonce,
+            'fee': self.fee,
+            'amount': self.amount,
+            'message': self.message,
+            'transaction_id': self.transaction_id
+        }
 
     
