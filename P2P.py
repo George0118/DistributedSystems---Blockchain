0bb5c75732efc5c520912a00282dd0b6d4abbb6e
FilipposSevastakis <106911339+FilipposSevastakis@users.noreply.github.com>
2024-03-16 19:33:21 +0200

    Merged main into sev_playground

--- P2P.py
+++ P2P.py
@@ -6,16 +6,13 @@ class P2P:
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
+        self.connections = []
         
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET: IPv4, SOCK_STREAM: TCP
-        self.connections = []
+        self.socket.bind((self.ip, self.port))
+        # self.socket.listen(10)  # 10: number of unaccepted connections that the system will allow before refusing new connections
 
         self.start()
-
-        # print(f"IP: {self.ip}, Port: {self.port}")
-        # print("Peers:")
-        # for peer in self.connections:
-        #     print(peer, end=" ")
 
     # Method to create connection with another node
     def connect(self, peer_ip, peer_port):
@@ -23,35 +20,42 @@ class P2P:
             connection = self.socket.connect((peer_ip, peer_port))
             self.connections.append(connection)
             print(f"Connected to {peer_ip}:{peer_port}")
+            new_connection = self.socket.recv(1024)
+            print(f"Received {new_connection}")
+            
         except socket.error as e:
             print(f"Failed to connect to {peer_ip}:{peer_port}. Error: {e}")
 
     # Method that binds the socket to the ip and port and starts listening for incoming connections
     def listen(self):
-        self.socket.bind((self.ip, self.port))
-        self.socket.listen(10)  # 10: number of unaccepted connections that the system will allow before refusing new connections
+        self.socket.listen(10)
         print(f"Listening for connections on {self.ip}:{self.port}")
-    
-        while True:
+        while (len(self.connections) < 9):
             connection, address = self.socket.accept()
-            self.connections.append(connection)
             print(f"Accepted connection from {address}")
+
+            # Broadcast the new connection to the other peers and, afterwards, add it to the connections list
+            self.data_exchange(connection)
+            # TODO: send to new connection the so-far connections
+            self.connections.append(connection)
+            
 
     # Method for exchanging data between peers
     def data_exchange(self, data):
         for connection in self.connections:
             try:
-                connection.sendall(data.encode())
+                connection.sendall(data)
             except socket.error as e:
                 print(f"Failed to send data. Error: {e}")
 
     # Multithreaded Node; in order to be able to both listen and send data simultaneously
     def start(self):
-        if (self.port == 47475):
-
+        if (self.port == 40000):
             listen_thread = threading.Thread(target=self.listen)
             listen_thread.start()
+            # exchange_thread = threading.Thread(target=self.exchange_thread, args=self.connections)
+            # exchange_thread.start()
         
         else:
-            listen_thread = threading.Thread(target=self.connect, args=("91.140.28.86", 47475))
+            listen_thread = threading.Thread(target=self.connect, args=("127.0.0.1", 40000))
             listen_thread.start()
