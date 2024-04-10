b3f880dd134318fa397187f9d0375301a289de62
geoka <geoka001@gmail.com>
2024-04-10 15:37:15 +0300

    config

--- running_script.py
+++ running_script.py
@@ -1,14 +1,19 @@
 import sys
 import threading
-from main import main
 import time
-from config import N
+import config
 
 if __name__ == '__main__':
     ip = sys.argv[1]
     base_port = int(sys.argv[2])
+    N = int(sys.argv[3])
+    CAPACITY = int(sys.argv[4])
     
+    config.N = N
+    config.CAPACITY = CAPACITY
     # Event to signal threads to exit
     stop_event = threading.Event()
 
-    main(ip, base_port, stop_event)
+    from node import Node
+
+    node = Node(ip, base_port, stop_event)
